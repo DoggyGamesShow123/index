2b18c15968d15418a9bf72b3fbc6c3f462ac7825
DoggyGamesShow123 <152580967+DoggyGamesShow123@users.noreply.github.com>
2026-04-23 17:08:05 +0000

    work

---
+++ commit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Simple commit script for site changes
+
+# Stage all changes
+git add -A
+
+# Commit with a message (use argument if provided, otherwise prompt)
+if [ -z "$1" ]; then
+  echo "change:"
+  read message
+else
+  message="$1"
+fi
+
+git commit -m "$message"
+
+# Ask if you want to push
+read -p "Push to remote? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git push
+fi
